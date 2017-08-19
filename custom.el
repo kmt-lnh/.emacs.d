70413ee21abf39c58afdbfdb6987ff42b4af0aae
kmt-lnh <kmt-lnh@users.noreply.github.com>
2017-08-19 22:26:13 +0200

    loading up ein for ipython

--- custom.el
+++ custom.el
@@ -21,7 +21,7 @@
     (org-bbdb org-bibtex org-docview org-gnus org-info org-irc org-mhe org-rmail org-w3m)))
  '(package-selected-packages
    (quote
-    (org-board haskell-mode jedi company-anaconda anaconda-mode py-autopep8 csv-mode elfeed elfeed-goodies elfeed-org emacsql multiple-cursors javadoc-lookup vlf dired+ bookmark+ undo-tree use-package expand-region omnisharp clojure-cheatsheet clojure-mode org-drill-table helm ess ess-R-object-popup ess-view ace-window z3-mode paredit magit helm-projectile fsharp-mode elm-mode auto-complete)))
+    (ein org-board haskell-mode jedi company-anaconda anaconda-mode py-autopep8 csv-mode elfeed elfeed-goodies elfeed-org emacsql multiple-cursors javadoc-lookup vlf dired+ bookmark+ undo-tree use-package expand-region omnisharp clojure-cheatsheet clojure-mode org-drill-table helm ess ess-R-object-popup ess-view ace-window z3-mode paredit magit helm-projectile fsharp-mode elm-mode auto-complete)))
  '(python-shell-interpreter "python")
  '(rng-nxml-auto-validate-flag nil)
  '(show-paren-mode t))
